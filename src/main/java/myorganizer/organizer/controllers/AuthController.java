8b387849f22c4f5ced11efb400e3f64e39af7a92
Yauhen <e.penkin@akveo.com>
2018-02-06 17:31:51 +0300

    feat(auth): logout feature add

--- src/main/java/myorganizer/organizer/controllers/AuthController.java
+++ src/main/java/myorganizer/organizer/controllers/AuthController.java
@@ -5,11 +5,8 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import myorganizer.organizer.models.AppUser;
 import myorganizer.organizer.models.TokenEntity;
 import myorganizer.organizer.repositories.UserRepository;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -44,36 +41,31 @@ public class AuthController {
     }
   }
 
-  @RequestMapping(value = "/sign-in", method = RequestMethod.POST, produces = "application/json")
+  @RequestMapping(value = "/sign-in", method = RequestMethod.POST)
   public @ResponseBody ResponseEntity<String> login(@RequestBody AppUser login) throws ServletException {
     String jwtToken = "";
-
     if (login.getEmail() == null || login.getPassword() == null) {
       throw new ServletException("Please fill in username and password");
     }
-
     String email = login.getEmail();
     String password = login.getPassword();
-
     AppUser user = userRepository.findByEmail(email);
-
     if (user == null) {
       throw new ServletException("User email not found.");
     }
-
     String pwd = user.getPassword();
-
     if (!bCryptPasswordEncoder.matches(password, pwd)) {
       throw new ServletException("Invalid login. Please check your name and password.");
     }
-
     jwtToken = Jwts.builder().setSubject(email).claim("roles", "user").setIssuedAt(new Date())
       .signWith(SignatureAlgorithm.HS256, "secretkey").compact();
 
-    final HttpHeaders httpHeaders = new HttpHeaders();
-    httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+    return new ResponseEntity<String>(new TokenEntity(jwtToken).toString(), HttpStatus.OK);
+  }
 
-    return new ResponseEntity<String>(new TokenEntity(jwtToken).toString() , httpHeaders, HttpStatus.OK);
+  @RequestMapping(value = "/sign-out", method = RequestMethod.DELETE)
+  public @ResponseBody HttpStatus logout() {
+    return HttpStatus.OK;
   }
 
   @GetMapping(path = "/all")
